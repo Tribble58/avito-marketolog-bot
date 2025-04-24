c57ad78cdecfdddc5c3bfaf0aeb7161313e44a85
Vladlen <vladlensakhnyuk@yandex.ru>
2025-04-24 20:32:56 +0300

    Created callbacks factories

--- src/callbacks.py
+++ src/callbacks.py
@@ -5,3 +5,9 @@ from aiogram.filters.callback_data import CallbackData
 
 class ChatsCallbackFactory(CallbackData, prefix="chat"):
     chat_id: str
+
+class MessagesCallbackFactory(CallbackData, prefix="message"):
+    chat_id: str
+
+class TemplatesCallbackFactory(CallbackData, prefix="template"):
+    template_text: str
